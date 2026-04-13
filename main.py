1f9cb35bb2dd22aeb9d212a906beeba869f2c028
PbILO <81964798+PbILO@users.noreply.github.com>
2026-04-13 07:09:01 +0500

    Update main.py
    1) Функция генерации реализована асинхронно
    2) Добавлены новые функции(команды) бота
    3) Добавлена обработка ошибок (исключений)

--- main.py
+++ main.py
@@ -1,17 +1,83 @@
 import discord
 from discord.ext import commands
 from google import genai
-
+import asyncio
 from config import *
 
 intents = discord.Intents().all()
 genaiClient = genai.Client(api_key=geminiToken)
 discordBot = commands.Bot(command_prefix=prefix, intents=intents)
 
+def generate(prompt: str) -> str:
+    GLOBAL_MEMORY = ('Отвечай на языке запроса без '
+                     'какого-либо специального форматирования, '
+                     'чтобы это можно было скопировать и отправить сообщением\n'
+                     'Твоё сообщение не должно превышать лимит СТРОГО 1950 и '
+                     'меньше символов. При этом если это код, ты можешь не писать его целиком, '
+                     'а просто пояснить его, если он слишком длинный. Но по возможности '
+                     'всегда отправляй код или решения целиком, если они вмещаются в лимит '
+                     '1950 символов (с пробелами)\n')
+    response = genaiClient.models.generate_content(model="gemini-3-flash-preview",
+                                                   contents=GLOBAL_MEMORY + prompt)
+    return response
+
+async def generate_async(prompt: str) -> str:
+    async with asyncio.Semaphore(1):
+        return await asyncio.to_thread(generate, prompt)
+
 @discordBot.command()
-async def ai(message, content):
-    response = genaiClient.models.generate_content(model="gemini-3-flash-preview", contents='ответь текстом (на языке запроса) без какого-либо специального форматирования, чтобы это можно было скопировать и отправить сообщением' + content)
-    await message.reply(response.text)
+async def ai(ctx, *content):
+    try:
+        response = await generate_async('ответь текстом (на языке запроса) '
+                            'без какого-либо специального '
+                            'форматирования, чтобы это можно'
+                            ' было скопировать и отправить'
+                            ' сообщением' + ' '.join(content))
+        await ctx.reply(response.text)
+    except Exception as e:
+        await ctx.reply('Ошибка на стороне Gemini. '
+                        'Возможно, истёк лимит или серверы перегружены. '
+                        f'Попробуйте задать вопрос позже. Ошибка {e}')
+
+@discordBot.command()
+async def retell(ctx, n: int = 50):
+    messages = []
+    async for message in ctx.channel.history(limit=n):
+        messages.append(message.author.name + ': ' + message.content + '\n')
+    try:
+        response = await generate_async('Сейчас ты увидишь диалог чата. '
+                            'Твоя задача кратко пересказать, '
+                            'о чём был диалог, '
+                            'чтобы не пришлось читать '
+                            'все эти сообщения.\n'
+                            + ''.join(messages[1:][::-1]))
+        print(messages)
+        await ctx.reply(response.text)
+    except Exception as e:
+        await ctx.reply('Ошибка на стороне Gemini. '
+                        'Возможно, истёк лимит или серверы перегружены. '
+                        f'Попробуйте задать вопрос позже. Ошибка {e}')
+
+@discordBot.command()
+async def explain(ctx):
+    if ctx.message.reference:
+        original = await ctx.fetch_message(ctx.message.reference.message_id)
+        try:
+            response = await generate_async('Сейчас ты увидишь что-то сложное. '
+                                'Если это код, то определи его язык, добавь комментарии и пояснения '
+                                'и объясни, какой функционал этого кода. Если же это сложные уравнения,'
+                                ' термины и тому подобное, то объясни всё современным языком, '
+                                'чтобы было максимально понятно. Можешь использовать шутки '
+                                'и мемы для наглядности. Сократи своё объяснение. '
+                                'Вот то, что надо проанализировать: \n'
+                                + original.content)
+            await ctx.reply(response.text)
+        except Exception as e:
+            await ctx.reply('Ошибка на стороне Gemini. '
+                            'Возможно, истёк лимит или серверы перегружены. '
+                            f'Попробуйте задать вопрос позже. Ошибка {e}')
+    else:
+        await ctx.reply('Нужно ответить на чьё-то сообщение, чтобы я знал, что я должен объяснить!')
 
 
 discordBot.run(discordToken)
