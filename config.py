66dce4fe0fb5c44d4cb6491fe1da2a9eefbdfc45
PbILO <81964798+PbILO@users.noreply.github.com>
2026-05-27 20:50:54 +0500

    Create config.py
    Added config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+discordToken = ''#<<<<ADD YOUR TOKEN
+geminiToken = ''#<<<<ADD YOUR TOKEN
+prefix = '/'
+discordAddBotToServerURL = ''#YOU DON'T NEED IT
+geminiModel='gemini-2.5-flash-lite'
+proxyServer='' #YOUR PROXY
