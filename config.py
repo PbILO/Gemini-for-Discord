65f3d7281ba56692d6539e099336a6656b29cbc2
Kachumbiy <ilyamalevich13@gmail.com>
2026-06-10 23:28:35 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 discordToken = ''#<<<<ADD YOUR TOKEN
-geminiToken = ''#<<<<ADD YOUR TOKEN
+geminiToken = 'AIzaSyDOPP11r9AJw9h1uwXYhsI4oFpnX4RxTgI'#<<<<ADD YOUR TOKEN
 prefix = '/'
+
+vkToken = 'vk1.a.NGTWGBg9sEI-IAXCByKVLte4qA_7WUYHBg5jDzfNgAspMt52p8i4vUaNrvbpjlFJVDozc0m5rXS8GtC0HgVJshlPEZjBIlf5vXSnQuhcc-JSGwNrEh06iCvJaXxAZtPgUYBTNd5hhcM_dQkZPqG7C0kR_Ac49b8vRl0dC1N0qVj-9CwXyAUiT7rsHJwmo-QEh-E3RA_I7mUMiSb-FmZg3g' #<<<<ADD YOUR TOKEN
+vkGroupId = 239491645  #<<<<ADD YOUR ID OF GROUP
+
 discordAddBotToServerURL = ''#YOU DON'T NEED IT
 geminiModel='gemini-2.5-flash-lite'
-proxyServer='' #YOUR PROXY
+proxyServer = 'http://82dnkE:oHKH66@196.19.120.143:8000' #YOUR PROXY
+#'http://82dnkE:oHKH66@196.19.120.143:8000'
